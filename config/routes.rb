ee65800720ea941a8ac6454375d486ff9f6fe28a
Jordi Pàmies <jordipamies2@gmail.com>
2017-01-04 23:34:05 +0000

    Creadas diferentes root url para el user que está sign in y para el que no lo está.

--- config/routes.rb
+++ config/routes.rb
@@ -1,59 +1,16 @@
 Rails.application.routes.draw do
+  # Creado por Devise para las urls /user/sign_in etc
   devise_for :users
-  # get 'welcome/index'
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  # Esta es la ruta para el user que está sign in
+  authenticated :user do
+    root 'welcome#index'
+  end
 
-  # You can have the root of your site routed with "root"
-  root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  # Esta es la ruta para el user que no está sign in
+  unauthenticated :user do
+    devise_scope :user do
+      root 'welcome#unregistered', as: :unregistered_root
+    end
+  end
 end
