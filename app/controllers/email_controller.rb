ea4fa03b5a29504059f8e8faa3df034eafb2b634
Jordi Pàmies <jordipamies2@gmail.com>
2017-01-06 15:27:26 +0000

    Creada acción create para ingresar los datos de email o mostrar lista de errores

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -1,4 +1,17 @@
 class EmailController < ApplicationController
+
+	
   def create
+  	# Aquí vamos a recibir los datos del campo email en el formualario email
+  	@email = EmailsList.new(email: params[:email])
+  	if @email.save
+  		# Si pasa las validaciones lo convierte en un objeto json
+  		render json: @email
+  	else
+  		# Si no pasa las validaciones es porque hay errores con la info que recibimos. en vez de hacer render del objeto hacemos render de un campo errors.
+  		render json: @email.errors, status: :unprocessable_entity
+  	end
+  	
+
   end
 end
