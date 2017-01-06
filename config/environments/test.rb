8ffc7a7886d594ec0b993afe75d37181a47ebc43
Jordi Pàmies <jordipamies2@gmail.com>
2017-01-06 14:51:04 +0000

    Configurado para poder hacer test desde la consola

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -37,6 +37,9 @@ Rails.application.configure do
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
+  # Requerido para hacer test desde la consola
+  config.web_console.development_only = false
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
