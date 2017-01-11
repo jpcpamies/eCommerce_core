0234bb4affec65338fec20af46621b4b3dcdf76c
Jordi Pàmies <jordipamies2@gmail.com>
2017-01-11 01:59:47 +0000

    Modificana la migración de product para añadir campo attachement para el avatar (requerido por paperclip) y añadida precisión al decimal del precio.

--- db/migrate/20170111015040_create_products.rb
+++ db/migrate/20170111015040_create_products.rb
@@ -2,10 +2,11 @@ class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
       t.string :name
-      t.decimal :pricing
+      t.decimal :pricing, scale: 2, precision: 10
       t.text :description
       t.references :user, index: true, foreign_key: true
-
+      # Requerido por paperclip
+      t.attachment :avatar
       t.timestamps null: false
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -31,11 +31,15 @@ ActiveRecord::Schema.define(version: 20170111015040) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
-    t.decimal  "pricing"
+    t.decimal  "pricing",             precision: 10, scale: 2
     t.text     "description"
     t.integer  "user_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
+    t.datetime "created_at",                                   null: false
+    t.datetime "updated_at",                                   null: false
   end
 
   add_index "products", ["user_id"], name: "index_products_on_user_id"
