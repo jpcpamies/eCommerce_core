af34002e7d19bd632a3b0da31bf2ddc0d46b75aa
Jordi Pàmies <jordipamies2@gmail.com>
2017-01-12 17:12:22 +0000

    Añaido callback en el controlador para validar que solo puedes hacer acciones con en el controlador attachment si eres el user owner del producto

--- app/controllers/attachment_controller.rb
+++ app/controllers/attachment_controller.rb
@@ -1,5 +1,6 @@
 class AttachmentController < ApplicationController
 	before_action :authenticate_user!
+	before_action :authenticate_owner!
   def new
   end
 
@@ -8,4 +9,13 @@ class AttachmentController < ApplicationController
 
   def destroy
   end
+
+  private
+  def authenticate_owner!
+		@product = Product.find(params[:product_id])
+		if @product.nill? || @product.user != current_user
+			redirect_to root_path, notice: "No puedes editar este producto"
+			return
+		end
+	end
 end
