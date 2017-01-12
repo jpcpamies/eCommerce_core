9003d2fc770773af4e1ad25b1a857509eb7396a4
Jordi Pàmies <jordipamies2@gmail.com>
2017-01-12 18:05:01 +0000

    Cambiar la ruta en la que paperclip guarda nuestros archivos

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,6 +1,7 @@
 class Attachment < ActiveRecord::Base
   belongs_to :product
   validates :archivo, attachment_presence: true
+  # Aquí le digo a paperclip en que ruta guardar los archivos adjuntos
   has_attached_file :archivo, path: ":rails_root/archivos/:id/:style/:filename"
 
   do_not_validate_attachment_file_type :archivo #Necesita cambiarse
