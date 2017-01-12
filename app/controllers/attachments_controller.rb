06d9581bc48bf8ae1f10c0ed67fa2b9fb8254e85
Jordi Pàmies <jordipamies2@gmail.com>
2017-01-12 17:44:15 +0000

    añadida funcionalidad en el controlador attachment para subir archivos adjuntos.

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -5,6 +5,12 @@ class AttachmentsController < ApplicationController
   end
 
   def create
+		@attachment = Attachment.new(attachment_params)
+  	if @attachment.save 
+  		redirect_to @attachment.product,notice: "Se guardo el archivo adjunto"
+  	else
+  		redirect_to @product, notice: "No pudimos guardar el archivo adjunto"
+  	end
   end
 
   def destroy
@@ -12,10 +18,14 @@ class AttachmentsController < ApplicationController
 
   private
   def authenticate_owner!
-		@product = Product.find(params[:product_id])
-		if @product.nill? || @product.user != current_user
-			redirect_to root_path, notice: "No puedes editar este producto"
+		@product = Product.find(params[:attachment][:product_id])
+		if @product.nil? || @product.user != current_user
+			redirect_to root_path, notice: "No puedes editar ese producto"
 			return
 		end
+	end
+
+	def attachment_params
+		params.require(:attachment).permit(:product_id,:archivo)
 	end
 end
